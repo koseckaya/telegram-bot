2c41cb731b62092ee75f085b4bde4067e49b1c40
koseckaya <kosetsaya@gmail.com>
2023-09-11 18:56:41 +0300

    feat: add .gitignore

--- index.js
+++ index.js
@@ -54,8 +54,7 @@ const start = () => {
         if (data === '/again') {
             return startGame(chatId)
         }
-        
-        if (data === chats[chatId]) {
+        if (data == chats[chatId]) {
             return bot.sendMessage(chatId, `Congratulations you're guess number ${data}`, againOptions)
         } else {
             return bot.sendMessage(chatId, `Sorry it's number ${chats[chatId]}`, againOptions)
